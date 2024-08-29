0a38c1024cd582020eeb76aac969317e3f687057
Nathan Guiard <nguiard@student.42.fr>
2024-08-29 14:33:27 +0200

    level09 source OK

---
+++ level09/source.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+typedef struct data {
+	char	msg[140];
+	char	usr[40];
+	short	msg_length;
+}	data;
+
+char *set_msg(data *d)
+{
+	char s[1024];
+
+	memset(s, 0, sizeof(s));
+	puts(">: Msg @Unix-Dude");
+	printf(">>: ");
+	fgets(s, 1024, stdin);
+	return strncpy(d->msg, s, d->msg_length); // last arg 140 ?
+}
+
+int set_username(data *d)
+{
+	char s[140];
+	int i;
+
+	memset(s, 0, 0x80uLL);
+	puts(">: Enter your username");
+	printf(">>: ");
+	fgets(s, 128, stdin);
+	for (i = 0; i <= 40 && s[i]; ++i)
+		d->usr[i] = s[i];
+	return printf(">: Welcome, %s", (const char *)(d->usr));
+}
+
+int handle_msg()
+{
+	data	d;
+
+	d.msg_length = 140;
+	set_username(&d);
+	set_msg(&d);
+	return puts(">: Msg sent!");
+}
+
+int main(int argc, const char **argv, const char **envp)
+{
+	puts(
+		"--------------------------------------------\n"
+		"|   ~Welcome to l33t-m$n ~    v1337        |\n"
+		"--------------------------------------------");
+	handle_msg();
+	return 0;
+}
